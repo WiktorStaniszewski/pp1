b9417f8357fcdca36401edc7902d8082f299a5b2
WiktorStaniszewski <wiktorst.8@gmail.com>
2023-10-15 10:05:39 +0200

    Chapter 2 programs

---
+++ My work/Chapter 2/Python1.py
@@ -0,0 +1,36 @@
+#Exercise 1
+5           #nothing
+x = 5       #assigns a value to a variable "x"
+x + 1       #adds one to a variable "x"
+
+#Exercise 2
+
+x = input("Please enter your name: ")
+print("Hello " + x)
+
+#Exercise 3
+
+hours = float(input("Please enter you hours: "))
+rate = float(input("Enter you rate: "))
+print(("Your pay is {}$").format(hours*rate))
+
+#Exercise 4
+
+width = 17
+height = 12.0
+
+a = print(width//2)     #  8 - int
+b = print(width/2)      #  8.5 - float
+c = print(height/3)     #  4.0 - float
+print(1+2*5)            #  11 - int
+
+#Exercise 5
+
+tempC = input("Type in the temperature in Celsius: ")
+try:
+    tempF = float(tempC)*1.8000 + 32.00
+except ValueError:
+    tempC = input("Type in the temperature in Celsius but with a dot: ")
+    tempF = float(tempC)*1.8000 + 32.00
+print(("The temperature is {}*C or {}*F").format(tempC, tempF))
+
